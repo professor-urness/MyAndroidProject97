2685905ef110e1837168d48da4123922f3187601
Professor Urness <professor.urness@gmail.com>
2015-09-07 13:39:58 -0500

    test

--- app/src/main/java/com/urness/myandroidproject97/MainActivity.java
+++ app/src/main/java/com/urness/myandroidproject97/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //This is a test
 
+        //and now another test
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
